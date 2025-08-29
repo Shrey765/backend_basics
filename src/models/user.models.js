4ba9b3ddb10328047bc692a24ac40917c0ec8a00
Shrey765 <shrey.eng72@gmail.com>
2025-08-29 09:25:05 +0530

    cloudinary user model schema methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -70,7 +70,13 @@ userSchema.methods.generateAccessToken = function(){
     })
 }
 userSchema.methods.generateRefreshToken = function(){
-    
+    return jwt.sign({
+        _id: this._id,
+    }, 
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: PerformanceObserverEntryList.env.REFRESH_TOKEN_EXPIRY
+    })
 }
 
 export const User = mongoose.model("User", userSchema)
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,26 @@
-const asyncHandler = () => {
+const asyncHandler = (fn) => {
+    async (req, resizeBy, next) => {
+        try {
+            await fn(req, resizeBy, next)
+        } catch (error) {
+            res.status(error.code || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
 }
 
-export {asyncHandler}
+export default asyncHandler;
+
+
+//agar promise k through krna ho to 
+{/*const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(
+            (err) => next(err)
+        )
+    }
+} 
+
+export default {asyncHandler}*/}
